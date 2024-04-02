4e14075971fd02bf799097cd53bcdab6810de3ac
Nurbek <117496813+NKey4@users.noreply.github.com>
2024-04-02 05:43:20 +0500

    asdaasd

--- src/pages/Appeals/index.jsx
+++ src/pages/Appeals/index.jsx
@@ -5,16 +5,19 @@ import styles from "./Appeals.module.scss";
 
 export const Appeals = () => {
   const appeals = useSelector(selectAppeals);
+  console.log(appeals);
   return (
-    <div className={style.appeals}>
+    <div className={styles.appeals}>
       <h1>Обращения</h1>
       <div className={styles.appeals_list}>
-        {appeals.map((appeal, index) => (
-          <div key={index} className={style.appeal_item}>
-            <h3>{appeal.question}</h3>
-            <p>{appeal.answer}</p>
-          </div>
-        ))}
+        {!appeals.length && <h3>Ваш список обращений пуст :( </h3>}
+        {appeals.length > 0 &&
+          appeals.map((appeal, index) => (
+            <div key={index} className={styles.appeal_item}>
+              <h3>{appeal._id + ":" + appeal.question}</h3>
+              <p>{appeal.answer}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
