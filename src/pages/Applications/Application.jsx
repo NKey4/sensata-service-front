65642dbf4d373fce71dd059b8bbb80ad63a25803
Nurbek <117496813+NKey4@users.noreply.github.com>
2024-04-18 04:51:25 +0500

    ФЫВФЫВФЫВ

--- src/pages/Applications/Application.jsx
+++ src/pages/Applications/Application.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import styles from "./Applications.module.scss";
 
+const getStatusColor = (status) => {
+  switch (status) {
+    case "Новая":
+      return "green";
+    case "В работе":
+      return "blue";
+    case "Исполнена":
+      return "grey";
+    case "Закрыта":
+      return "black";
+    case "Отказана":
+      return "red";
+    case "Отменена":
+      return "orange";
+    default:
+      return "black"; // Default color if the status is unrecognized
+  }
+};
+
 export const Application = ({ id, dataMessage, createdAt, status }) => {
   return (
     <div className={styles.Application}>
       <div className={styles.Head}>
         <h3 className={styles.ApplicationId}>{id}</h3>
-        <li>{status}</li>
+        <li style={{ color: getStatusColor(status) }}>{status}</li>
       </div>
 
       <p className={styles.ApplicationData}>{dataMessage}</p>
